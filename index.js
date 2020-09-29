224433e476ef0a5061890287ee81a5c643a03e3a
HMoreira2020 <moreiraheather@gmail.com>
2020-09-28 20:18:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2 
+let set = [3, 5, 12,]
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() + 1)
+
+let mod = 40 % 6 
+
+let max = Math.max(3, 5, 12, 20);
